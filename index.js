1b95f49b510b713fd92c6f1d1971b1ad613d8d73
fallahX <75850671+fallahX@users.noreply.github.com>
2020-12-23 21:31:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Jul
 const bulan = arrayBulan[moment().format('MM') - 1]
 
 const config = {
-    BotName: 'ALF BOT',
-    instagram: 'https://instagram.com/aditiaalfians',
-    whatsapp: 'wa.me/6285799496179',
-    kapanbotaktif: '24 JAM',
+    BotName: 'PUDIDI-BOTV1',
+    kapanbotaktif: 'KALO ADA SINYAL BAGUS:D',
     tanggal: `TANGGAL: ${moment().format('DD')} ${bulan} ${moment().format('YYYY')}`,
     waktu: time
 }
